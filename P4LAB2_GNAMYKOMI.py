5e6438a2b29b8d1d349eaccbcf61d0fe9ec711b5
gnamyk6261 <145700157+gnamyk6261@users.noreply.github.com>
2023-12-11 17:02:19 -0500

    Add files via upload

---
+++ P4LAB2_GNAMYKOMI.py
@@ -0,0 +1,9 @@
+''' Type your code here. '''
+first = int(input())
+second = int(input())
+if second < first :
+     print("Second integer can't be less than the first.")
+else: 
+     for i in range  (first,second+1,5):
+        print(i, end=" ")
+     print()
