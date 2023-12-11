c9ce41d7d59dfed00e825108beb0aa0207011118
gnamyk6261 <145700157+gnamyk6261@users.noreply.github.com>
2023-12-11 16:45:43 -0500

    Add files via upload

---
+++ P2LAB2_GNAMYKOMI.py
@@ -0,0 +1,11 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+''' Type your code here. '''
+product_1= num1*num2*num3*num4
+average_2 =((num1+num2+num3+num4)/4)
+
+print(f'{product_1:.0f} {average_2:.0f}')
+print(f'{product_1:.3f} {average_2:.3f}')
